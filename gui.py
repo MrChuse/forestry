33d731d67c0de0f99b9e1441df9d3270f7ce6047
MrChuse <aleksha007@mail.ru>
2022-05-23 22:17:34 +0300

    Implemented Take all button in the ApiaryWindow

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import math
 import time
+import os.path
 
 import pygame
 import pygame_gui
@@ -311,10 +312,11 @@ class UIRelativeStatusBar(UIStatusBar):
 
 class ApiaryWindow(UIWindow):
     initial_position = (0, 0)
-    def __init__(self, apiary: Apiary, cursor: Cursor, manager, *args, **kwargs):
+    def __init__(self, game: Game, apiary: Apiary, cursor: Cursor, manager, *args, **kwargs):
+        self.game = game
         self.apiary = apiary
         self.cursor = cursor
-        self.size = (300, 400)
+        self.size = (300, 420)
         super().__init__(pygame.Rect(self.initial_position, self.size), manager, "Apiary " + apiary.name, *args, **kwargs)
 
         self.button_size = (60, 60)
@@ -351,6 +353,16 @@ class ApiaryWindow(UIWindow):
             rect.x += dx
             rect.y += dy
             self.buttons.append(UIButton(rect, '', manager, self))
+        take_all_button_rect = pygame.Rect(0, 0, self.size[0] - self.side_margin2 * 2, 30)
+        take_all_button_rect.centerx = rect.centerx - 40 # no clue why 40
+        self.take_all_button = UIButton(take_all_button_rect, 'Take all', manager, self,
+            anchors={
+                'left': 'left',
+                'right': 'right',
+                'top': 'top',
+                'bottom': 'bottom',
+                'top_target': self.buttons[3]
+            })
     
     def set_button_texts(self):
         self.princess_button.set_text(self.apiary.princess.small_str())
@@ -367,7 +379,9 @@ class ApiaryWindow(UIWindow):
     
     def process_event(self, event: pygame.event.Event) -> bool:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == self.princess_button:
+            if event.ui_element == self.take_all_button:
+                self.game.take(self.apiary.name, '0..6')
+            elif event.ui_element == self.princess_button:
                 if self.cursor.slot.is_empty():
                     self.cursor.slot.put(self.apiary.take_princess())
                 else:
@@ -523,7 +537,7 @@ class ResourcePanel(UIPanel):
                 if event.text != 'Build':
                     apiary = self.game.build(event.text.lower())
                     if apiary is not None:
-                        ApiaryWindow(apiary, self.cursor, self.ui_manager)
+                        ApiaryWindow(self.game, apiary, self.cursor, self.ui_manager)
                     self.game.update_apiary_list()
         elif event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == self.forage_button:
@@ -542,7 +556,7 @@ class GUI(Game):
         self.resource_panel = ResourcePanel(self, self.cursor, pygame.Rect(0, 0, resource_panel_width, window_size[1]), 0, manager)
         self.apiary_windows = []
         ApiaryWindow.initial_position = (resource_panel_width, 0)
-        api_window = ApiaryWindow(self.apiaries[0], self.cursor, manager)
+        api_window = ApiaryWindow(self, self.apiaries[0], self.cursor, manager)
         self.apiary_windows.append(api_window)
         right_text_box_rect = pygame.Rect(0, 0, resource_panel_width, window_size[1])
         right_text_box_rect.right = 0
@@ -569,9 +583,10 @@ class GUI(Game):
 
         r = pygame.Rect(0,0,window_size[0]/2, window_size[1]/2)
         r.center = (window_size[0]/2, window_size[1]/2)
-        help_window = UIWindow(r, manager)
-        help_text = UITextBox(
-            '''Welcome to a demo of my little game about selective breeding!
+        if not os.path.exists('save.forestry'):
+            help_window = UIWindow(r, manager)
+            help_text = UITextBox(
+                '''Welcome to a demo of my little game about selective breeding!
 You are going to be a professional bee breeder and your goal for this demo is to build an alveary.
 In order to do this you'll have to (surprisingly) breed the bees you have and try to find the pairs of species that result in a mutation after mating.
 
@@ -582,9 +597,12 @@ Bees have genes, and you only see their phenotype at first; to discover their ge
 You can build new apiaries using `Build` button on the left. When you build an alveary, you win the game!
 
 Also, you can change the size of the inventory window (if you have lower resolution, you may not see the bees' species and sex)
-On the right side you can see logs which show when something went wrong and some other information'''.replace('\n', '<br>'),
+On the right side you can see logs which show when something went wrong and some other information
+
+Don't forget to save your progress using `Save` button located at the top of your inventory.
+You can load your saved progress. Only one save slot is available (you can manipulate save.forestry file however)'''.replace('\n', '<br>'),
                 pygame.Rect(0,0, r.width-32, r.height-59), manager, container=help_window
-        )
+            )
 
     def render(self):
         pass
@@ -605,7 +623,7 @@ On the right side you can see logs which show when something went wrong and some
             if event.ui_element == self.apiary_selection_list:
                 index = int(event.text.split()[-1])
                 self.apiary_windows.append(
-                    ApiaryWindow(self.apiaries[index], self.cursor, self.ui_manager)
+                    ApiaryWindow(self, self.apiaries[index], self.cursor, self.ui_manager)
                 )
 
 def main():
