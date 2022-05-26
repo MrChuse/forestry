7156873f799fd461915ed3de4d71fdd07bbd4cf0
MrChuse <aleksha007@mail.ru>
2022-05-26 12:47:22 +0300

    Added gradient to buttons

--- gui.py
+++ gui.py
@@ -58,6 +58,10 @@ class Cursor(UIButtonSlot):
         pos = pygame.mouse.get_pos()
         self.relative_rect.topleft = pos
         self.rect.topleft = pos
+        if self.slot.is_empty():
+            self.rect.size = (-1, -1)
+        else:
+            self.rect.size = (64, 64)
         return super().update(time_delta)
 
 class InventoryWindow(UIWindowNoX):
