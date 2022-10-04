54e5f449402762a3c80c291d6de48cdd17c67904
ALukashevichus <aleksha007@mail.ru>
2022-10-04 17:27:19 +0300

    Moved UIWindowNoX to its own file

--- gui.py
+++ gui.py
@@ -33,14 +33,6 @@ def process_cursor_slot_interaction(event, cursor, slot):
             cursor.slot.take()
 
 
-
-class UIWindowNoX(UIWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.enable_close_button = False
-        self.title_bar_close_button_width = 0
-        self.rebuild()
-
 class UIButtonSlot(UIButton):
     empty_object_id = '#EMPTY'
     def __init__(self, slot: Slot, *args, highlighted=False, **kwargs):
