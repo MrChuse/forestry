323298bec5ab27a7dc81f46da2a42e55d7ad1149
MrChuse <aleksha007@mail.ru>
2023-01-27 02:00:15 +0300

    Better placement of buttons in InspectPopup

--- ui/game_components/ui_button_slot.py
+++ ui/game_components/ui_button_slot.py
@@ -3,22 +3,33 @@ from typing import Dict, Tuple, Union
 
 import pygame
 import pygame_gui
-from pygame_gui.core import ObjectID, UIContainer
+from pygame_gui.core import ObjectID, UIContainer, UIElement
 from pygame_gui.elements import UIButton, UIStatusBar, UITextBox, UITooltip
 
 from config import local
 from forestry import Bee, Drone, Princess, Queen, Slot
 
 from ..custom_events import INSPECT_BEE
+from ..elements import UICheckbox
 from .bee_stats import BeeStats
 from .tutorial_stage import CurrentTutorialStage, TutorialStage
 
 
 class InspectPopup(UITooltip):
     def __init__(self, bee_button: 'UIButtonSlot', hover_distance: Tuple[int, int], manager=None, parent_element = None, object_id: Union[ObjectID, str, None] = None, anchors: Dict[str, str] = None):
-        super().__init__('', hover_distance, manager, parent_element, object_id, anchors)
+        self.container = None
         self.bee_button = bee_button
-        if not bee_button.bee_inspectable:
+        super().__init__('', hover_distance, manager, parent_element, object_id, anchors)
+        self.rebuild()
+
+    def rebuild(self):
+        super().rebuild()
+
+        if self.container is not None:
+            self.container.kill()
+            self.container = None
+
+        if not self.bee_button.bee_inspectable:
             width = 318
             height = 177
         else:
@@ -27,50 +38,46 @@ class InspectPopup(UITooltip):
         self.top_margin = 4
         self.inspect_button_height = 32
 
-        self.container = UIContainer(pygame.Rect(0, 0, width, height), manager, starting_height=self.ui_manager.get_sprite_group().get_top_layer()+1, parent_element=self)
+        self.container = UIContainer(pygame.Rect(0, 0, width, height), self.ui_manager, starting_height=self.ui_manager.get_sprite_group().get_top_layer()+1, parent_element=self)
         bee_stats_rect = pygame.Rect(0, self.top_margin, width, height)
         self.inspect_button = None
         if self.bee_button.is_inspectable and CurrentTutorialStage.current_tutorial_stage >= TutorialStage.INSPECT_AVAILABLE:
-            self.inspect_button = UIButton(pygame.Rect(0, self.top_margin, width - self.inspect_button_height, self.inspect_button_height), local['Inspect'], manager, self.container)
-            self.inspect_button.set_hold_range((2, 2))
             bee_stats_rect.top += self.inspect_button_height
 
         self.bee_stats = BeeStats(self.bee_button.slot.slot, bee_stats_rect, container=self.container, parent_element=self, resizable=True)
 
-        self.pin_button_unpinned = UIButton(pygame.Rect(width - self.inspect_button_height,
+        self.pin_button = UICheckbox(pygame.Rect(-self.inspect_button_height,
                                                         self.top_margin,
                                                         self.inspect_button_height,
                                                         self.inspect_button_height),
-                                            '', manager, self.container, object_id='#unpinned', starting_height=2)
-        self.pin_button_pinned = UIButton(pygame.Rect(width - self.inspect_button_height,
-                                                      self.top_margin,
-                                                      self.inspect_button_height,
-                                                      self.inspect_button_height),
-                                          '', manager, self.container, object_id='#pinned', starting_height=2)
-        self.pin_button_pinned.hide()
-        self.pinned = False
+                                            False, '', container=self.container, object_id='#unpinned', starting_height=2,
+                                            anchors={
+                                                'left': 'right',
+                                                'right': 'right'
+                                            })
+        if self.bee_button.is_inspectable and CurrentTutorialStage.current_tutorial_stage >= TutorialStage.INSPECT_AVAILABLE:
+            self.inspect_button = UIButton(pygame.Rect(0, self.top_margin, width - self.inspect_button_height, self.inspect_button_height), local['Inspect'], container=self.container,
+                anchors={
+                    'left': 'left',
+                    'right': 'right',
+                    'right_target': self.pin_button
+                })
+            self.inspect_button.set_hold_range((2, 2))
 
         xdim = self.bee_stats.rect.size[0]
         ydim = self.bee_stats.rect.size[1] + self.top_margin
         if self.inspect_button is not None:
             ydim += self.inspect_button.rect.size[1]
         self.container.set_dimensions((xdim, ydim))
-        super().set_dimensions((xdim, ydim))
-        self.text_block.kill()
+        UIElement.set_dimensions(self, (xdim, ydim))
+        if self.text_block is not None:
+            self.text_block.kill()
 
     def process_event(self, event: pygame.event.Event) -> bool:
         if event.type == pygame_gui.UI_BUTTON_START_PRESS:
             if event.ui_element == self.inspect_button:
                 event_data = {'ui_element': self}
                 pygame.event.post(pygame.event.Event(INSPECT_BEE, event_data))
-            elif event.ui_element == self.pin_button_unpinned:
-                self.pinned = True
-                self.pin_button_unpinned.hide()
-                self.pin_button_pinned.show()
-            elif event.ui_element == self.pin_button_pinned:
-                self.pinned = False
-                self.pin_button_pinned.hide()
-                self.pin_button_unpinned.show()
         return super().process_event(event)
 
     def kill(self):
@@ -198,7 +205,7 @@ class UIButtonSlot(UIButton):
             self.saved_amount = self.slot.amount
 
         if (self.inspect_popup is not None and
-                not self.inspect_popup.pinned and
+                not self.inspect_popup.pin_button.checked and
                 not self.hover_point(*self.ui_manager.get_mouse_position()) and
                 not self.inspect_popup.container.hover_point(*self.ui_manager.get_mouse_position())):
             self.inspect_popup.kill()
