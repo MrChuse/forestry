504e7728f550c486dcf0e484e622c668253eb52c
MrChuse <aleksha007@mail.ru>
2023-01-27 01:00:23 +0300

    Added checked text to UICheckbox

--- ui/elements/ui_checkbox.py
+++ ui/elements/ui_checkbox.py
@@ -7,11 +7,12 @@ from pygame_gui.elements import UIButton
 
 
 class UICheckbox(UIButton):
-    def __init__(self, relative_rect: pygame.Rect, checked: bool, manager, container = None, tool_tip_text: Union[str, None] = None, starting_height: int = 1, parent_element = None, object_id: Union[ObjectID, str, None] = None, anchors: Dict[str, str] = None, allow_double_clicks: bool = False, generate_click_events_from = frozenset([pygame.BUTTON_LEFT]), visible: int = 1):
+    def __init__(self, relative_rect: pygame.Rect, checked: bool, checked_text='✓', manager=None, container = None, tool_tip_text: Union[str, None] = None, starting_height: int = 1, parent_element = None, object_id: Union[ObjectID, str, None] = None, anchors: Dict[str, str] = None, allow_double_clicks: bool = False, generate_click_events_from = frozenset([pygame.BUTTON_LEFT]), visible: int = 1):
         self.checked = checked
+        self.checked_text = checked_text
         if object_id is None:
             object_id = '#checkbox'
-        super().__init__(relative_rect, '✓' if self.checked else '', manager, container, tool_tip_text, starting_height, parent_element, object_id, anchors, allow_double_clicks, generate_click_events_from, visible)
+        super().__init__(relative_rect, self.checked_text if self.checked else '', manager, container, tool_tip_text, starting_height, parent_element, object_id, anchors, allow_double_clicks, generate_click_events_from, visible)
         if self.checked:
             self.select()
 
@@ -22,7 +23,7 @@ class UICheckbox(UIButton):
                 self.checked = not self.checked
                 if self.checked:
                     self.select()
-                    self.set_text('✓')
+                    self.set_text(self.checked_text)
                 else:
                     self.unselect()
                     self.set_text('')
