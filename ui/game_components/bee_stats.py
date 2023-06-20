271325dde921d335a4cbdfb3c4f8a665a34e9916
MrChuse <aleksha007@mail.ru>
2023-06-21 02:03:20 +0300

    Rewrote bee stats using UITable

--- ui/game_components/bee_stats.py
+++ ui/game_components/bee_stats.py
@@ -3,44 +3,35 @@ from typing import Dict, List, Optional, Union
 import pygame
 import pygame_gui
 from pygame_gui.core import ObjectID, UIElement
+from pygame_gui.core.interfaces import IContainerLikeInterface, IUIManagerInterface
 from pygame_gui.elements import UIButton, UILabel, UIPanel
 
 from config import UI_MESSAGE_SIZE, dominant, genes_enums, local
 from forestry import Bee, dom_local
 
 from ..custom_events import TUTORIAL_STAGE_CHANGED
-from ..elements import UILocationFindingMessageWindow
+from ..elements import UILocationFindingMessageWindow, UITable
 from .tutorial_stage import CurrentTutorialStage, TutorialStage
 
 
 def colorize(text, color):
     return f'<font color={color}>{text}</font>'
 
-class BeeStats(UIPanel):
-    def __init__(self, bee: Bee, relative_rect: pygame.Rect, manager=None, wrap_to_height: bool = False, layer_starting_height: int = 1, container = None, parent_element = None, object_id: Union[ObjectID, str, None] = None, anchors: Dict[str, str] = None, visible: int = 1, resizable=False):
+class BeeStats(UITable):
+    def __init__(self, bee: Bee, relative_rect: pygame.Rect, starting_layer_height: int = 1, manager: IUIManagerInterface | None = None, *, element_id: str = 'panel', margins: Dict[str, int] | None = None, container: IContainerLikeInterface | None = None, parent_element: UIElement | None = None, object_id: ObjectID | str | None = None, anchors: Dict[str, str | UIElement] | None = None, visible: int = 1, resizable: bool = False):
         self.bee = bee
         self.resizable = resizable
         self.table_contents : Optional[List[List[UIElement]]] = None
         self.original_rect = relative_rect
         self.buttons = []
-        super().__init__(relative_rect, layer_starting_height, manager, container=container, parent_element=parent_element, object_id=object_id, anchors=anchors, visible=visible)
+        super().__init__(relative_rect, starting_layer_height, manager, element_id=element_id, margins=margins, container=container, parent_element=parent_element, object_id=object_id, anchors=anchors, visible=visible, resizable=resizable)
         self.rebuild()
 
-    def rebuild(self):
-        super().rebuild()
-
+    def populate_table_contents(self):
         if self.table_contents is not None:
             for row in self.table_contents:
                 for element in row:
                     element.kill()
-
-        if self.bee is None:
-            if hasattr(self, 'panel_container'):
-                self.set_dimensions(self.original_rect.size)
-            return
-        if not hasattr(self, 'panel_container'):
-            # has not __init__ed yet, skipping
-            return
 
         def create_uilabel(text='', is_local=False, object_id=None, visible=True):
             return UILabel(pygame.Rect(0,0,-1,-1), local[text] if is_local else text, container=self, object_id=ObjectID('@SmallFont', object_id), visible=visible)
@@ -49,7 +40,11 @@ class BeeStats(UIPanel):
             b._gene_name = gene_name
             self.buttons.append(b)
             return b
+
         self.table_contents = []
+        if self.bee is None:
+            return # set table contents to []
+
         if not self.bee.inspected:
             self.table_contents.append([create_uilabel(self.bee.small_str())])
         else:
@@ -71,32 +66,10 @@ class BeeStats(UIPanel):
                                             create_uilabel(dom_local(allele0, dom0), False, '@Dominant' if dom0 else '@Recessive'),
                                             create_uilabel(dom_local(allele1, dom1), False, '@Dominant' if dom1 else '@Recessive')])
 
-        top_margin = 2
-        heights = [max(map(lambda x: x.get_abs_rect().height, row)) + top_margin for row in self.table_contents]
-        left_margin = 2
-        transposed = zip(*self.table_contents)
-        widths = [max(map(lambda x: x.get_abs_rect().width, row)) + left_margin for row in transposed]
-        # print(len(widths), len(heights))
-        # for row_n in range(len(res)):
-        #     for el_n in range(len(res[row_n])):
-        #         print(f'({widths[el_n]:4} {heights[row_n]:4})', end=' ')
-        #     print()
-        # print()
-
-        for row_n in range(len(self.table_contents)):
-            for el_n in range(len(self.table_contents[row_n])):
-                center_x = sum(widths[:el_n]) + widths[el_n]/2 + left_margin * (el_n + 1)
-                center_y = sum(heights[:row_n]) + heights[row_n]/2 + top_margin * (row_n + 1)
-                element = self.table_contents[row_n][el_n]
-                rel_rect = element.get_relative_rect()
-                rel_rect.center = center_x, center_y
-                element.set_relative_position(rel_rect.topleft)
-
-        if self.resizable:
-            new_dimensions = (sum(widths) + left_margin * (len(self.table_contents[0]) + 1) + (self.container_margins['left'] + self.container_margins['right']),
-                            sum(heights) + top_margin * (len(self.table_contents) + 1) + (self.container_margins['top'] + self.container_margins['bottom']))
-
-            self.set_dimensions(new_dimensions)
+    def rebuild(self):
+        if hasattr(self, 'panel_container'):
+            self.populate_table_contents()
+        super().rebuild()
 
     def open_gene_helper(self, gene):
         if CurrentTutorialStage.current_tutorial_stage == TutorialStage.INSPECT_AVAILABLE:
